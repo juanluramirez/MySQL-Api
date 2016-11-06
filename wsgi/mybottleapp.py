1e692cb4bd61af5d631e6520aeb282baaeaab9d5
Juan Luis Ramirez Vaquero <juanlu@Juanlu-PC>
2016-11-06 10:57:21 +0100

    Modificando mybootleapp.py

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from bottle import route, run, template, get, post, request, response, redirect, default_app, static_file, TEMPLATE_PATH, error, redirect
-
+import MySQLdb
 
 @route('/')
 def redirect_to_login():
-	redirect('/login')
+	redirect('/index')
+
+@route('/index', method=["GET"])
+def login():
+	return template('index.tpl')
