c2a3aa30d994d71f81b119a73da9417f2bf9d78d
Juan Luis Ramirez Vaquero <juanlu@Juanlu-PC>
2016-11-06 11:31:11 +0100

    Añadido acceso.tpl y modificado mybootleapp.py

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -8,5 +8,16 @@ def redirect_to_login():
 	redirect('/index')
 
 @route('/index', method=["GET"])
-def login():
+def index():
 	return template('index.tpl')
+
+@route('/acceso', method='POST')
+def acceso():
+	host  = request.forms.get("host")
+	user  = request.forms.get("user")
+	passwd = request.forms.get("passwd")
+	db= request.forms.get("db")
+	basedatos = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db)
+	cursor = basedatos.cursor()
+	tablas = cursor.execute("show tables")
+	return template('acceso.tpl', db=db, tablas=tablas)
---
+++ wsgi/views/acceso.tpl
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head> 
+		<link rel="shortcut icon" type="image/x-icon" href="https://upload.wikimedia.org/wikipedia/en/6/62/MySQL.svg" />
+		<title>Acceso</title> 
+		<link rel="stylesheet" href="/static/css/perfil.css" />
+	</head> 
+<body>
+	<h1>Base de datos: {{db}}.</h1>
+	<h2>Tablas :</h2>
+	<h3>{{tablas}}</h3> 
+</body>  
+</html>
