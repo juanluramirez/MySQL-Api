7df892b1001a625fbac60b3c31e8c95f9cb6cc7d
Juan Luis Ramirez Vaquero <juanlu@Juanlu-PC>
2016-11-06 17:24:00 +0100

    Modificado mybootleapp.py

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -25,4 +25,24 @@ def acceso():
 	numero_tablas=len(resultados)
 	return template('acceso.tpl', db=db, tablas=resultados, numero_tablas=numero_tablas)
 
+@route('/acceso', method=["GET"])
+def acceso():
+	return template('acceso.tpl')
+
+@route('/consulta', method='POST')
+def consulta():
+	host  = request.forms.get("host")
+	user  = request.forms.get("user")
+	passwd = request.forms.get("passwd")
+	db= request.forms.get("db")
+	Datos  = request.forms.get("datos")
+	Tabla  = request.forms.get("tabla")
+	basedatos = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db)
+	cursor = basedatos.cursor()
+	tablas = cursor.execute("select %s from  %s"(Datos,Tabla))
+	resultados = cursor.fetchall()
+	cursor.close()
+	numero_tablas=len(resultados)
+	return template('consulta.tpl', consulta=resultados, tabla=Tabla)
+
 run(host='127.0.0.1', port=8080, debug=True)
--- wsgi/views/acceso.tpl
+++ wsgi/views/acceso.tpl
@@ -11,5 +11,17 @@
 	<h2>Tablas :</h2>
 	<h3>{{tablas}}</h3>
 	<h3>Tiene un total de: {{numero_tablas}} tablas 
+	<div id="search-form">
+		<form class="form-container" action="/consulta" method="post">
+		<p>Datos: <input name="datos" type="text" class="search-field"/>
+		<p>Tabla: <input name="tabla" type="text" class="search-field"/>
+		<p>Host: <input name="host" type="text" class="search-field"/>
+		<p>Usuario: <input name="user" type="text" class="search-field"/>
+		<p>Contraseña: <input name="passwd" type="password" class="search-field"/>
+		<p>Base de datos: <input name="db" type="text" class="search-field"/>
+		<div class="submit-container">
+			<input type="submit" value="Acceder" class="submit" />
+		</div>
+	</div> 
 </body>  
 </html>
